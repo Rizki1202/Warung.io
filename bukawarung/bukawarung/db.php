d07ae51d73f640179e7bf5c628b3fe079648356d
Rizki1202 <121491483+Rizki1202@users.noreply.github.com>
2022-12-29 08:31:20 +0700

    Add files via upload

---
+++ bukawarung/bukawarung/db.php
@@ -0,0 +1,8 @@
+<?php
+    $hostname = 'localhost';
+    $username= 'host';
+    $password = 'o=11%Wpqy1AVm7#N';
+    $dbname = 'db_bukawarung';
+
+    $conn = mysqli_connect($hostname, $username, $password, $dbname) or die ('Gagal terhubung ke database');
+?>
